e3671cdeab634f12714be15dd1015b4bf9792cbc
John Lombardo <john@programbo.com>
2016-04-28 02:44:50 +0800

    Use the full airbnb config again

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const airbnbEslintConfig = require('eslint-config-airbnb/base');
-const extend = require('extend');
 const fs = require('fs');
 const path = require('path');
-const overrideConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '.eslintrc')));
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, '.eslintrc')));
 
-module.exports = extend(true, {}, airbnbEslintConfig, overrideConfig);
+module.exports = config;
